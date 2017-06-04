80c65205a83e58160369806471bdf981c3bfa26a
Andy Matuschak <andy@khanacademy.org>
2017-06-04 13:47:58 -0700

    Commiting stray changes for partitive division

--- Sketches/Partitive division with stretching.framer/framer/framer.generated.js
+++ Sketches/Partitive division with stretching.framer/framer/framer.generated.js
@@ -7,7 +7,7 @@ if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":1,"select
 }
 if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":1,"selectedHand":"","deviceType":"fullscreen","contentScale":1,"orientation":0};
 }
-window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"Division with Stretching.framer"};
+window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"Partitive division with stretching.framer"};
 
 Framer.Device = new Framer.DeviceView();
 Framer.Device.setupContext();
